4fdd68303e0ab136530e6b2073df165b20e231ac
Luis <luis@mokleta.(none)>
2010-04-12 20:12:32 -0300

    * Catch several exceptions * Fixed some bugs

--- bin/vacation.py
+++ bin/vacation.py
@@ -15,13 +15,19 @@ import syslog
 syslog.openlog('vacation', syslog.LOG_PID)
 
 
-vacation_conf = "/usr/local/simple_vacation/config/vacation.cfg"
+vacation_conf = "/home/luis/vacation.cfg"
+#vacation_conf = "/usr/local/simple_vacation/config/vacation.cfg"
+
+def log(level, message):
+  if (level == syslog.LOG_ERR) or (verbose == True):
+    syslog.syslog(level|syslog.LOG_MAIL, message)
+
 
 config = ConfigParser.RawConfigParser()
 try:
   config.read(vacation_conf)
 except ConfigParser.MissingSectionHeaderError:
-  print "Section Global is missing in configuration file\n"
+  log(syslog.LOG_ERR, "Section Global is missing in configuration file")
   sys.exit(1)
 
 
@@ -41,12 +47,12 @@ def read_config(parameter):
     elif parameter == "ldap_port":
       return 389
     else:
-      print "Parameter %s is missing" % parameter
+      log(syslog.LOG_ERR, "Parameter %s is missing" % parameter)
       sys.exit(1)
 
 config_parameters = ["verbose", "vacation_home", "vacation_log_path", "ldap_host", "ldap_port", "ldap_base", "default_vacation_message"]
 for parameter in config_parameters:
-  parameter = read_config(parameter)
+  vars()[parameter] = read_config(parameter)
 
 
 def pair_exists(local_addr, remote_addr):
@@ -65,9 +71,6 @@ def add_pair(local_addr, remote_addr):
   log.write("\n")
   log.close()
 
-def log(level, message):
-  if level == syslog.LOG_ERR or verbose:
-    syslog.syslog(level|syslog.LOG_MAIL, message)
   
 
 raw_email = sys.stdin.read()
@@ -78,15 +81,24 @@ from_addr = Utils.parseaddr(msg.__getitem__('From'))
 to_addr = Utils.parseaddr(msg.__getitem__('To'))
 subject = msg.__getitem__('Subject')
 
-l = ldap.initialize('ldap://%s:%d' % (ldap_host, ldap_port))
+l = ldap.initialize('ldap://%s:%d' % (ldap_host, int(ldap_port)))
 
-vacation_search = l.search_s(ldap_base, ldap.SCOPE_SUBTREE, "(mail=%s)" % to_addr[1], ['vacationActive', 'vacationInfo'])
+try:
+  vacation_search = l.search_s(ldap_base, ldap.SCOPE_SUBTREE, "(mail=%s)" % to_addr[1], ['vacationActive', 'vacationInfo'])
+except ldap.NO_SUCH_OBJECT:
+  log(syslog.LOG_ERR, "The user with email: %s was not found in the LDAP tree" % to_addr[1])
+  sys.exit(1)
+except ldap.SERVER_DOWN:
+  log(syslog.LOG_ERR, "Can't connect to ldap server on %s address and %s port" % (ldap_host, ldap_port.__str__()))
+  sys.exit(1)
 
-if not vacation_search:
+if not vacation_search[0][1].has_key('vacationActive'):
+  log(syslog.LOG_INFO, "The user %s has not the LDAP attribute vacationActive set" % to_addr[1])
   sys.exit()
 
 if vacation_search[0][1]['vacationActive'][0] == 'TRUE':
   if pair_exists(to_addr[1], from_addr[1]):
+    log(syslog.LOG_INFO, "Pair already exists. Don't send auto-respond")
     sys.exit()
   
   if vacation_search[0][1].has_key('vacationInfo'):
@@ -102,8 +114,15 @@ if vacation_search[0][1]['vacationActive'][0] == 'TRUE':
   response_email['To'] = from_addr[1]
   
   s = smtplib.SMTP('localhost')
-  s.sendmail(to_addr[1], from_addr[1], response_email.as_string())
+
+  try:
+    s.sendmail(to_addr[1], from_addr[1], response_email.as_string())
+  except:
+    log(syslog.LOG_ERR, "It wasn't possible to send the email. See the mail system log for more information")
+
   s.quit
+  
+  log(syslog.LOG_INFO, "Auto-respond sent to %s" % from_addr[1])
   
   add_pair(to_addr[1], from_addr[1])
 
